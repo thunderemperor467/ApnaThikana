6980ab3bd727077d562734d28aba7a31448f504f
thunderemperor467 <thunderemperor467@gmail.com>
2025-07-31 23:01:51 +0530

    .

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use("/", userRouter);
 
 // ✅ Root route for homepage
 app.get("/", (req, res) => {
-    res.render("index.js"); 
+    res.render("index.ejs"); 
 });
 
 // Dummy route for testing
